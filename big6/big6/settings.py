910f84ce7c711f855cf03e01c54b144ff31ecd92
KanhaKanhaiya <100350283+KanhaKanhaiya@users.noreply.github.com>
2022-05-24 12:26:55 +0530

    Update settings.py

--- big6/big6/settings.py
+++ big6/big6/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6uhepai_vl$-=h8b$a6sp%$6ck2=rw)t9-_q7#ncef$hncqupy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
