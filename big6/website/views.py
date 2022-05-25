ebb889c0becb9bd592cb85b1b6638151dd3d1c8d
KanhaKanhaiya <100350283+KanhaKanhaiya@users.noreply.github.com>
2022-05-25 14:48:11 +0530

    Added Navbar

--- big6/website/views.py
+++ big6/website/views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def homePage(request):
-    return render(request, "index.html")
+    data = {"title":"The big6 Project", "body":"Welcome to The big6 Project"}
+    return render(request, "index.html", data)
